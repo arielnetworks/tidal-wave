6223a23b28de81cddf2447317f5599791bb8ff8d
ikezoe_a <ikezoe_a@ariel-networks.com>
2014-08-01 14:48:53 +0900

    画像を重ねて表示してみたり

--- public/client/app.js
+++ public/client/app.js
@@ -1,17 +1,21 @@
 angular.module('app', [])
   .controller('myController', ['$scope', function ($scope) {
 
-    $scope.data = [];
+    $scope.items = [];
     var socket = io('ws://10.0.2.90:5000');
-    socket.on('message', function(data) {
+    socket.on('message', function(msg) {
       $scope.$apply(function(){
-        $scope.data = data.vector;
+        if (msg.status == 'SUSPICIOUS') {
+          $scope.items.push(msg);
+        }
       });
     });
 
     socket.send({
-      'expect_image': '/home/ikezoe_a/test_images/widget_common_normal',
-      'target_image': '/home/ikezoe_a/test_images/widget_common_normal_scratch',
+      'expect_path': 'public/test_images/widget_common_normal',
+      'target_path': 'public/test_images/widget_common_normal_scratch',
+      //'expect_path': 'public/images',
+      //'target_path': 'public/images2',
       'threshold': 5,
       'span': 10
     });
@@ -20,46 +24,66 @@ angular.module('app', [])
   .directive('flow', [function () {
     return {
       scope: {
-        height: '=',
-        width: '=',
         data: '='
       },
       restrict: 'E',
       link: function(scope, element) {
-        var svg = d3.select(element[0])
-          .append('svg')
-          .attr('width', scope.width)
-          .attr('height', scope.height);
-
-        svg.append("image")
-          .attr("xlink:href", "/images/customjsp1.png")
-          .attr("width", scope.width)
-          .attr("height", scope.height);
-
 
         scope.$watch('data', function(newval){
-          console.time("link");
-          svg.selectAll('.vector-line')
-            .data(scope.data)
-            .enter()
-            .append('svg:line')
-            .attr('class', 'vector-line')
-            .attr({
-              x1: function(d) {
-                return d.x;
-              },
-              y1: function(d) {
-                return d.y;
-              },
-              x2: function(d) {
-                return d.x + d.dx;
-              },
-              y2: function(d) {
-                return d.y + d.dy;
-              },
-              stroke: 'black'
-            });
-          console.timeEnd("link");
+
+          var img = new Image();
+          var expect_img = scope.data.expect_image.replace('public', 'http://10.0.2.90:5000');
+          var target_img = scope.data.target_image.replace('public', 'http://10.0.2.90:5000');
+          img.src = expect_img;
+          img.onload = function () {
+
+            var width = this.width;
+            var height = this.height;
+            var svg = d3.select(element[0])
+              .append('svg')
+              .attr('width', width)
+              .attr('height', height);
+
+            var svg_expect_img = svg.append("image")
+              .attr("image-rendering", "optimizeQuality")
+              .attr("x", '0')
+              .attr("y", '0');
+
+            svg_expect_img.attr('height', height)
+               .attr('width', width)
+               .attr('xlink:href', expect_img);
+
+            var svg_target_img = svg.append("image")
+              .attr("image-rendering", "optimizeQuality")
+              .attr("x", '0')
+              .attr("y", '0')
+              .attr("opacity", '0.5');
+
+            svg_target_img.attr('height', height)
+               .attr('width', width)
+               .attr('xlink:href', target_img);
+
+            svg.selectAll('.vector-line')
+              .data(scope.data.vector)
+              .enter()
+              .append('svg:line')
+              .attr('class', 'vector-line')
+              .attr({
+                x1: function(d) {
+                  return d.x;
+                },
+                y1: function(d) {
+                  return d.y;
+                },
+                x2: function(d) {
+                  return d.x + d.dx;
+                },
+                y2: function(d) {
+                  return d.y + d.dy;
+                },
+                stroke: 'red'
+              });
+          };
         });
 
       }
