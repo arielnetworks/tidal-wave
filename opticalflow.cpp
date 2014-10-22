d933b00dd5af85b4140a23010dfed18a2ce4f13f
ikezoe_a <ikezoe_a@ariel-networks.com>
2014-10-22 20:52:45 +0900

    GPUを使わないでもビルドできるように

--- opticalflow.cpp
+++ opticalflow.cpp
@@ -5,11 +5,14 @@
 #include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/video/video.hpp"
-#include "opencv2/gpu/gpu.hpp"
 
 using namespace std;
 using namespace cv;
+
+#ifdef USE_GPU
+#include "opencv2/gpu/gpu.hpp"
 using namespace cv::gpu;
+#endif
 
 float calcOpticalFlow(string expectImgPath, string targetImgPath, bool gpuMode, Mat &flowx, Mat &flowy) {
   if (expectImgPath.empty()) cout << "Specify left image path\n";
@@ -32,32 +35,46 @@ float calcOpticalFlow(string expectImgPath, string targetImgPath, bool gpuMode, 
     targetImg = resizedTargetImg;
   }
 
+  double pyrScale = 0.5;
+  int numLevels = 3;
+  int winSize = 30;
+  int numIters = 3;
+  int polyN = 7;
+  double polySigma = 1.5;
+  int flags = OPTFLOW_FARNEBACK_GAUSSIAN;
+
+#ifdef USE_GPU
   GpuMat d_frameL(expectImg), d_targetImg(targetImg);
   GpuMat d_flowx, d_flowy;
   FarnebackOpticalFlow d_calc;
+
+  d_calc.pyrScale = pyrScale;
+  d_calc.numLevels = numLevels;
+  d_calc.winSize = winSize;
+  d_calc.numIters = numIters;
+  d_calc.polyN = polyN;
+  d_calc.polySigma = polySigma;
+  d_calc.flags = flags;
+#endif
+
   Mat flowxy;
 
   int64 tc0, tc1;
 
-  d_calc.pyrScale = 0.5;
-  d_calc.numLevels = 3;
-  d_calc.winSize = 30;
-  d_calc.numIters = 3;
-  d_calc.polyN = 7;
-  d_calc.polySigma = 1.5;
-  d_calc.flags = OPTFLOW_FARNEBACK_GAUSSIAN;
 
   if (gpuMode) {
+#ifdef USE_GPU
     tc0 = getTickCount();
     d_calc(d_frameL, d_targetImg, d_flowx, d_flowy);
     tc1 = getTickCount();
     d_flowx.download(flowx);
     d_flowy.download(flowy);
+#endif
   } else {
     tc0 = getTickCount();
     calcOpticalFlowFarneback(
-                expectImg, targetImg, flowxy, d_calc.pyrScale, d_calc.numLevels, d_calc.winSize,
-                d_calc.numIters, d_calc.polyN, d_calc.polySigma, d_calc.flags);
+                expectImg, targetImg, flowxy, pyrScale, numLevels, winSize,
+                numIters, polyN, polySigma, flags);
     tc1 = getTickCount();
 
     Mat planes[] = {flowx, flowy};
